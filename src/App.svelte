2d07fa26b4ce505849b532da909a8dd6c5e965b7
Wade Moore <wmoore@capsher.com>
2023-09-01 14:37:06 -0500

    readme changes

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,11 @@
   import { environment } from './environments/environment';
   import Map from './lib/Map.svelte';
 	export let ready: boolean;
-
-  console.log(environment.apiKey);
 </script>
 
 <svelte:head>
 	<script defer async
-	src="https://maps.googleapis.com/maps/api/js?key={environment.apiKey}&callback=initMap">
+	src="https://maps.googleapis.com/maps/api/js?key={environment.googleMapsApiKey}&callback=initMap">
 	</script>
 </svelte:head>
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     envName: 'prod',
-    apiKey: ''
+    googleMapsApiKey: '',
+    eBirdsApiKey: '',
 };
